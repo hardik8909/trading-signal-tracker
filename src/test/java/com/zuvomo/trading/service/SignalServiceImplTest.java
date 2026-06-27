da0e49c824b6041b8ccf4637a26242c5a54c967f
Hardik <hardikdang2005@gmail.com>
2026-06-27 09:46:28 +0530

    Fix unit tests

--- src/test/java/com/zuvomo/trading/service/SignalServiceImplTest.java
+++ src/test/java/com/zuvomo/trading/service/SignalServiceImplTest.java
@@ -10,8 +10,10 @@ import com.zuvomo.trading.exception.ResourceNotFoundException;
 import com.zuvomo.trading.repository.TradingSignalRepository;
 import com.zuvomo.trading.service.impl.SignalServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -21,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class SignalServiceImplTest {
     @Mock
     private TradingSignalRepository repository;
